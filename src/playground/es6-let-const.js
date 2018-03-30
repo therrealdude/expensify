ccc23d9c44139158bbbda579ed9b5d786f94b470
therrealdude <dandersonerling@gmail.com>
2018-03-30 09:12:09 -0500

    adding project

--- src/playground/es6-let-const.js
+++
@@ -1,8 +0,0 @@
-var nameVar = 'Andrew';
-console.log('nameVar', nameVar);
-
-let nameLet = 'Jeremy';
-console.log('nameLet', nameLet);
-
-const nameConst = 'Sally';
-console.log('nameConst', nameConst);
