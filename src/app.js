fdd586361dd1e3e92b0a52f888b62d5619cd5561
Daniel Anderson <danderson@adagetech.com>
2018-04-20 11:03:26 -0500

    updated packages and removed things

--- src/app.js
+++ src/app.js
@@ -13,19 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(()=> {
-});
-
-store.dispatch(addExpense({ description: 'Water Bill', amount:300 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount:500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount:1500 }));
-store.dispatch(setTextFilter('bill'));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('gas'));
-}, 3000);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
