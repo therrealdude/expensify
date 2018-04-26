a33e3a87e2dcee06932cca29e7496226b4364c43
Daniel Anderson <danderson@adagetech.com>
2018-04-26 17:54:28 -0500

    added setting expenses store from db

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -18,6 +18,26 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+      snapshot.forEach((childSnapshot) => {
+      expenses.push({
+       id: childSnapshot.key,
+       ...childSnapshot.val()
+      });
+      dispatch(setExpenses(expenses));
+     });
+   });
+ }
+};
+
 export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
     const {
